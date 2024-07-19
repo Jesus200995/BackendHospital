6458843e6350dcda2341ea3cb0b0d0914c43dd02
Jess <84833058+Jesus200995@users.noreply.github.com>
2024-07-19 14:20:45 -0600

    Cambios

--- config/db.py
+++ config/db.py
@@ -2,8 +2,21 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://avnadmin:AVNS_VD6_sm2LUwoFm7Fmlw@test-utxicotepec-8d70.h.aivencloud.com:10780/defaultdb?ssl-mode=REQUIRED"
+# URL de conexión a la base de datos
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://avnadmin:AVNS_VD6_sm2LUwoFm7Fmlw@test-utxicotepec-8d70.h.aivencloud.com:10780/defaultdb"
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+# Configuración SSL
+ssl_args = {
+    'ssl': {
+        'ssl_ca': '/path/to/ca.pem',
+        'ssl_cert': '/path/to/client-cert.pem',
+        'ssl_key': '/path/to/client-key.pem'
+    }
+}
+
+# Crear el motor con configuración SSL
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args=ssl_args)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+
