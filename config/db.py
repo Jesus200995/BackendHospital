3a847bda42561df08197a20e690413ae5020bb60
Jess <84833058+Jesus200995@users.noreply.github.com>
2024-07-19 14:33:28 -0600

    2

--- config/db.py
+++ config/db.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://avnadmin:AVNS_VD6_sm2LUwoFnm7Fmlw@test-utxicotepec-8d70.h.aivencloud.com:10780/defaultdb?ssl-mode=REQUIRED"
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://avnadmin:AVNS_VD6_sm2LUwoFnm7Fmlw@test-utxicotepec-8d70.h.aivencloud.com:10780/defaultdb"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
